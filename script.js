f40c197ebe824f2ead7684b4699d0bacaa654fa5
kundan <candyboy6646@gmail.com>
2021-07-19 14:38:33 +0530

    added skills section

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ mybutton = document.getElementById("scrollBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
